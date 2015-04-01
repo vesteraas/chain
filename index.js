85bb2e258a1a8d1075fd72d876446f2958e20174
Werner Vesterås <wvesteraas@gmail.com>
2015-04-01 14:53:08 +0200

    Misc
    -Improved error handling
    -Better description
    -Test data
    -License
    -Repo URL

--- index.js
+++ index.js
@@ -42,19 +42,36 @@ module.exports = function () {
 
     return {
         in: function (stream) {
+            if (!stream || !(
+                stream instanceof Stream.Stream &&
+                typeof (stream._read === 'function')&&
+                typeof (stream._readableState === 'object')
+                )
+            ) {
+                throw 'The argument to in() should be a readable stream';
+            }
             inStream = stream;
             return this;
         },
         read: function (file) {
+            if (!(typeof file == 'string' || file instanceof String)) {
+               throw 'The argument to read() should be a string';
+            }
             inStream = fs.createReadStream(file);
             return this;
         },
         execute: function (command) {
-            executions.push({command: command});
+            if (!(typeof command == 'string' || command instanceof String)) {
+                throw 'The argument to execute() should be a string';
+            }
 
+            executions.push({command: command});
             return this;
         },
         write: function (file) {
+            if (!(typeof file == 'string' || file instanceof String)) {
+                throw 'The argument to write() should be a string';
+            }
             process();
 
             var outStream = inStream;
